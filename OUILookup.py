b1959f75840f5bc31d65f9e360c8408face96bf3
nhicocotapia <nicolas.tapiaga@alumnos.uv.cl>
2024-11-05 20:52:26 -0300

    OUILookup.py

--- OUILookup.py
+++ OUILookup.py
@@ -96,6 +96,6 @@ def mostrar_tabla_arp():
         print("Error al obtener la tabla ARP:", e)
 
 # Corregir la línea de inicialización
-if _name_ == "_main_":
+if __name__ == "_main_":
     programa_principal(sys.argv[1:])
  
